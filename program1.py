e9df8db4c4e9ea17cec16941a088867baa95c8a2
banoththirupathi <banoththirupathinayak944@gmail.com>
2024-11-11 15:01:48 +0530

    both program1 and program2 completed

--- program1.py
+++ program1.py
@@ -1,10 +1,17 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        matching_bracket = {')': '(', '}': '{', ']': '['}
+        stack = []
+
+        for char in s:
+            if char in matching_bracket:
+                top_element = stack.pop() if stack else '#'
+                if matching_bracket[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
 
 
 
--- program2.py
+++ program2.py
@@ -1,10 +1,12 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        for i in range(len(s)):
+            current_val = roman_map[s[i]]
+            next_val = roman_map[s[i + 1]] if i + 1 < len(s) else 0
+            total += -current_val if current_val < next_val else current_val
+        return total
 
 
 
